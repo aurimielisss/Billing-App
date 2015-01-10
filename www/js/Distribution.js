fee0e534746abd8b691c60d2b2f883b056c5a4f2
Gavin Power <gavinpower747@hotmail.com>
2015-01-10 01:38:21 +0000

    Initial Commit

---
+++ www/js/Distribution.js
@@ -0,0 +1,24 @@
+
+
+
+$(function () {
+
+    $.connection.hub.url = "http://37.187.35.32:8081/signalr";
+    $.connection.hub.logging = true;
+    var distributionHub = $.connection.distributionHub;
+
+    distributionHub.client.displayBills = function(Bills) {
+        for(var bill in Bills)
+            self.bills.push(new Bill(Bills[bill].Type, Bills[bill].DueDate.split("T")[0], Bills[bill].Amount));
+    }
+
+    function Bill(type, date, amount) {
+        this.type = type;
+        this.date = date;
+        this.amount = amount;
+    }
+
+    $.connection.hub.start({jsonp: true}).done(function() {
+       distributionHub.server.getBills();
+    });
+})
---
+++ www/js/index.js
@@ -0,0 +1,30 @@
+
+var app = {
+
+    initialize: function() {
+        this.bindEvents();
+    },
+
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+        var ViewModel = function() {
+            var self = this;
+            self.bills = ko.observableArray();
+        }
+        ko.applyBindings(ViewModel);
+    },
+
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
